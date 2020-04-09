51fdaf066f163bf357f3f1c43ad15d5992e2d7cc
Tom Brien <tom@byth.co.uk>
2020-04-09 14:39:20 +0100

    Cryo notes
    Add notes on cryogenic performance and stability

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3', 'mdx'],
-    'Design And Operaion': ['doc1'],
+    'Performance': ['cooldown', 'tempStability'],
     Analysis: ['spectrum', 'noise']
   },
 };
