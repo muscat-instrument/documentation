abd4447b3c92fd6e3b5216c33f020d995f857716
Tom Brien <tom@byth.co.uk>
2020-04-15 15:24:35 +0100

    Overview skeleton
    Very basics to create an overview page

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/overview')}>
               Get Started
             </Link>
           </div>
