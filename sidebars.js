f66e6e7996f913b9137a05830f3c081ce5f3ec2c
Tom Brien <TomBrien@users.noreply.github.com>
2021-03-16 15:07:33 +0000

    Add troubleshooting for diode indexing (#72)
    * Add troubleshooting for diode indexing
    
    * Fix dead link

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
   someSidebar: {
     Overview: ['overview'],
-    Design: [{'Mechanical': ['opening', 'closing']}, 'design', {'Cryogenics': ['recycling']}],
-    Interface: [{'Cryogenics': ['cryo-UI', 'cryo-API']}],
+    Design: [{ 'Mechanical': ['opening', 'closing'] }, 'design', { 'Cryogenics': ['recycling'] }],
+    Interface: [{ 'Cryogenics': ['cryo-UI', 'cryo-API', 'cryo-troubleshooting'] }],
     Analysis: ['spectrum', 'noise'],
     Performance: ['cooldown', 'tempStability']
-    },
+  },
 };
