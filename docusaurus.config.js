de8ed92960543afd52a9e893360e4e0407e3ee52
Tom Brien <TomBrien@users.noreply.github.com>
2021-05-19 10:51:52 +0100

    Add menu bar item

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,11 @@ module.exports = {
           position: 'left',
         },
         {
+          href: 'gallery/',
+          label: 'Gallery',
+          position: 'right',
+        },
+        {
           href: 'https://github.com/muscat-instrument',
           label: 'GitHub',
           position: 'right',
